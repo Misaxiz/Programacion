c69ff65a9b7a64857ecd37cd3f8b0edc65b770cb
Misaxiz <misael230406@gmail.com>
2024-07-22 19:58:36 -0600

    Update CalcularSalarioNeto.java

--- CalcularSalarioNeto.java
+++ CalcularSalarioNeto.java
@@ -1,22 +1,22 @@
 import java.util.Scanner;
-
 public class CalcularSalarioNeto {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Ingrese el salario bruto mensual: ");
-        double salarioBruto = scanner.nextDouble();
-
-        System.out.print("Ingrese el porcentaje de impuestos: ");
-        double porcentajeImpuestos = scanner.nextDouble();
-
-        System.out.print("Ingrese las deducciones adicionales: ");
-        double deduccionesAdicionales = scanner.nextDouble();
-
-        double impuesto = salarioBruto * (porcentajeImpuestos / 100);
-
-        double salarioNeto = salarioBruto - impuesto - deduccionesAdicionales;
-
-        System.out.println("Salario Neto: " + salarioNeto);
-    }
+ public static void main(String[] args) {
+ Scanner scanner = new Scanner(System.in);
+     
+ System.out.print("Ingrese el salario bruto mensual: ");
+ double salarioBruto = scanner.nextDouble();
+     
+ System.out.print("Ingrese el porcentaje de impuestos: ");
+ double porcentajeImpuestos = scanner.nextDouble();
+     
+ System.out.print("Ingrese las deducciones adicionales: ");
+ double deduccionesAdicionales = scanner.nextDouble();
+     
+ double impuestos = salarioBruto * (porcentajeImpuestos / 100);
+ double salarioNeto = salarioBruto - impuestos -
+     
+deduccionesAdicionales;
+ System.out.printf("El salario neto es: %.2f%n", salarioNeto);
+ scanner.close();
+ }
 }
